021cabf996a196e7533ec6f3015dc96ebd5d5252
sadik5397@gmail.com </student/>
2022-12-05 23:49:01 +0600

    App Feedback

--- lib/component/flat_avatar.dart
+++ lib/component/flat_avatar.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+
 import '../theme/border_radius.dart';
 import '../theme/colors.dart';
 import '../theme/padding_margin.dart';
@@ -11,7 +12,13 @@ Container flatAvatar({required BuildContext context, required String label}) {
       decoration: BoxDecoration(color: const Color(0xFF3498DB).withOpacity(.3), borderRadius: halfOfPrimaryBorderRadius),
       height: 88,
       width: 88,
-      child: Text(label, style: Theme.of(context).textTheme.displaySmall?.copyWith(fontWeight: FontWeight.bold, color: primaryBlack)));
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text("FLAT", style: Theme.of(context).textTheme.titleSmall?.copyWith(fontWeight: FontWeight.bold, color: primaryBlack)),
+          Text(label, style: Theme.of(context).textTheme.displaySmall?.copyWith(fontWeight: FontWeight.bold, color: primaryBlack)),
+        ],
+      ));
 }
 
 Container utilityAvatar({required BuildContext context, String? photo, required String type, double? size}) {
@@ -19,7 +26,7 @@ Container utilityAvatar({required BuildContext context, String? photo, required 
       margin: EdgeInsets.only(right: primaryPaddingValue),
       alignment: Alignment.center,
       decoration: BoxDecoration(color: const Color(0xFF3498DB).withOpacity(.3), borderRadius: halfOfPrimaryBorderRadius, image: photo != null ? DecorationImage(image: CachedNetworkImageProvider(photo)) : null),
-      height: size??88,
-      width: size??88,
+      height: size ?? 88,
+      width: size ?? 88,
       child: photo == null ? Icon(Icons.construction, color: primaryTitleColor, size: 36) : null);
 }
--- lib/views/car_parking/parking_booking.dart
+++ lib/views/car_parking/parking_booking.dart
@@ -1,8 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:hi_society_device/component/app_bar.dart';
 import 'package:hi_society_device/component/parking_booking.dart';
 import 'package:hi_society_device/theme/padding_margin.dart';
-import '../../component/overstay_list_tile.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../api/api.dart';
+import '../../component/snack_bar.dart';
 
 class ParkingBooking extends StatefulWidget {
   const ParkingBooking({Key? key}) : super(key: key);
@@ -14,17 +21,55 @@ class ParkingBooking extends StatefulWidget {
 class _ParkingBookingState extends State<ParkingBooking> {
   //variable
   TextEditingController searchController = TextEditingController();
+  String accessToken = "";
+  List apiResult = [];
+  List bookingList = [];
+
+  //APIs
+  Future<void> readAmenityBookingByDate({required String accessToken, required String date}) async {
+    try {
+      var response = await http.post(Uri.parse("$baseUrl/amenity-booking/list/amenity/booking/by-date?date=$date"), headers: authHeader(accessToken));
+      Map result = jsonDecode(response.body);
+      print(result);
+      if (result["statusCode"] == 200 || result["statusCode"] == 201) {
+        showSnackBar(context: context, label: result["message"]);
+        setState(() => apiResult = result["data"]);
+      } else {
+        showSnackBar(context: context, label: result["message"][0].toString().length == 1 ? result["message"].toString() : result["message"][0].toString());
+      }
+    } on Exception catch (e) {
+      showSnackBar(context: context, label: e.toString());
+    }
+  }
+
+  //Functions
+  Future<void> onRefresh() async {
+    await defaultInit();
+  }
+
+  defaultInit() async {
+    final pref = await SharedPreferences.getInstance();
+    setState(() => accessToken = pref.getString("accessToken")!);
+    await readAmenityBookingByDate(accessToken: accessToken, date: DateFormat('yyyy-M-dd').format(DateTime.now()));
+  }
+
+//Initiate
+  @override
+  void initState() {
+    super.initState();
+    defaultInit();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: primaryAppBar(context: context, title: "Car Park Bookings"),
-        body: (false)
+        body: (apiResult.isEmpty)
             ? const Center(child: CircularProgressIndicator())
             : ListView.builder(
                 shrinkWrap: true,
                 padding: primaryPadding * 2,
-                itemCount: 50,
+                itemCount: apiResult.length,
                 itemBuilder: (context, index) => parkBookingListTile(
                   context: context,
                   title: "MD. Zulfiaqar Sayeed",
