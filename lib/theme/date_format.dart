40820b96d7fe7b7ee30df8ffb4913a6cf53c8e37
sadik5397@gmail.com </student/>
2023-02-21 15:57:10 +0600

    GMT Fixed

--- lib/theme/date_format.dart
+++ lib/theme/date_format.dart
@@ -1,5 +1,10 @@
 import 'package:intl/intl.dart';
 
-String primaryDateTime(String dateTime) => DateFormat('dd MMM yyyy | hh:mm a').format(DateTime.parse(dateTime));
-String primaryDate(String dateTime) => DateFormat('dd MMM yyyy').format(DateTime.parse(dateTime));
-String primaryTime(String dateTime) => DateFormat('hh:mm a').format(DateTime.parse(dateTime));
+// String primaryDateTime(String dateTime) => DateFormat('dd MMM yyyy | hh:mm a').format(DateTime.parse(dateTime));
+// String primaryDate(String dateTime) => DateFormat('dd MMM yyyy').format(DateTime.parse(dateTime));
+// String primaryTime(String dateTime) => DateFormat('hh:mm a').format(DateTime.parse(dateTime));
+
+//GMT+6
+String primaryDateTime(String dateTime) => DateFormat('dd MMM yyyy | hh:mm a').format(DateTime.parse(dateTime).add(const Duration(hours: 6)));
+String primaryDate(String dateTime) => DateFormat('dd MMM yyyy').format(DateTime.parse(dateTime).add(const Duration(hours: 6)));
+String primaryTime(String dateTime) => DateFormat('hh:mm a').format(DateTime.parse(dateTime).add(const Duration(hours: 6)));
