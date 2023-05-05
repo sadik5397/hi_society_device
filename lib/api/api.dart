1ee305556b0866f370e425f42dc94c60b53c8dbd
sadik5397@gmail.com </student/>
2023-05-05 15:42:43 +0600

    Fixed Timezone

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,9 +1,9 @@
 
 // String baseUrl = "https://hisocietyserver.online"; //LiveServer
-// String baseUrl = "https://dev.hisocietyserver.online"; //DemoServer
+String baseUrl = "https://dev.hisocietyserver.online"; //DemoServer
 
 // String baseUrl = "http://192.168.68.125:3000"; //Localhost
-String baseUrl = "https://g01.fusionbdtech.com"; //TestServer
+// String baseUrl = "https://g01.fusionbdtech.com"; //TestServer
 // String baseUrl = "http://194.163.40.107:4441"; //LiveServer
 // String baseUrl = "http://10.0.2.2:3000"; //LocalhostAndroidEmulator
 Map<String, String> primaryHeader = {"Content-Type": "application/json", "charset": "utf-8"};
